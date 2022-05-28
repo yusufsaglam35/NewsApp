4f32b49a7692da81df4751230f393e1fb31b2686
Yusuf Sağlam <yusufsaglam.web@gmail.com>
2022-05-28 12:41:59 +0300

    NewsApp
    Öğrenmeye devam

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { Text, StyleSheet, View } from 'react-native'
+import React, { Component } from 'react'
+
+const App =() => {
+     return (
+      <View>
+        <Text>App</Text>
+      </View>
+    )
+}
+
+export default App;
+const styles = StyleSheet.create({})
